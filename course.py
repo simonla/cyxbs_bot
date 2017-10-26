cc0e28fc03c851aa78805559846cd5aff15237db
zengmingyang96@gmail.com <zengmingyang96@gmail.com>
2017-10-26 23:05:11 +0800

    fixed a time bug

--- course.py
+++ course.py
@@ -9,13 +9,13 @@ class course(object):
         if time == "三四节":
             self.lesson = '10:15~11:55'
         if time == "五六节":
-            self.lesson = '14:00~9:40'
+            self.lesson = '14:00~13:40'
         if time == "七八节":
-            self.lesson = '16:15~5:55'
+            self.lesson = '16:15~17:55'
         if time == "九十节":
             self.lesson = '19:00~20:40'
         if time == "十一二":
-            self.lesson = '21:00~我也不知道'
+            self.lesson = '21:00~22:40'
 
     def get_course(self):
         return "⌚" + self.lesson + " ==> " + self.name + '@' + self.classroom + '\n\n'
